67a0a89e8c54bea0d6f683abc843844b23373e48
Jason Wells <flipstock@gmail.com>
2017-11-20 18:15:48 -0700

    raw_token export for client

--- lib/jet/client.rb
+++ lib/jet/client.rb
@@ -6,10 +6,13 @@ module Jet
   class Client
     API_URL = 'https://merchant-api.jet.com/api'.freeze
 
-    def initialize(config = {})
+    def initialize(config = {}, raw_token = {})
       @api_user = config[:api_user]
       @secret = config[:secret]
       @merchant_id = config[:merchant_id]
+      @id_token = raw_token[:id_token]
+      @token_type = raw_token[:token_type]
+      @expires_on = raw_token[:expires_on]
     end
 
     def encode_json(data)
@@ -20,8 +23,8 @@ module Jet
       Oj.load(json)
     end
 
-    def token
-      unless @id_token && @token_type && @expires_on > Time.now
+    def generate_token
+      unless token_valid?
         body = { user: @api_user, pass: @secret }
         response = RestClient.post("#{API_URL}/token", encode_json(body))
         parsed_response = decode_json(response.body)
@@ -29,8 +32,28 @@ module Jet
         @token_type = parsed_response['token_type']
         @expires_on = Time.parse(parsed_response['expires_on'])
       end
+      raw_token
+    end
 
+    def token
+      generate_token unless token_valid?
       { Authorization: "#{@token_type} #{@id_token}" }
+    end
+
+    def raw_token
+      if token_valid?
+        {
+          id_token: @id_token,
+          token_type: @token_type,
+          expires_on: @expires_on
+        }
+      else
+        {}
+      end
+    end
+
+    def token_valid?
+      @id_token && @token_type && @expires_on > Time.now
     end
 
     def rest_get_with_token(path, query_params = {})
--- ruby-jet.gemspec
+++ ruby-jet.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |gem|
   gem.name        = 'ruby-jet'
-  gem.version     = '0.10.0'
+  gem.version     = '0.11.0'
   gem.date        = '2017-06-02'
   gem.summary     = 'Jet API for Ruby'
   gem.description = 'Jet API service calls implemented in Ruby'
